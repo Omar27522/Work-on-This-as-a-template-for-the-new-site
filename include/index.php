c85b21557b41bc2d3b31fc15584bfc9efae65f1e
Omar27522 <m1i6g5u9e8l2@gmail.com>
2024-11-23 07:58:23 -0800

    include
    intro

--- include/index.php
+++ include/index.php
@@ -150,14 +150,25 @@ function content(){ ?>
                 <?php
                     button('#design');
                 ?><br />
+                <h2>TO-DO LIST 11/21/2024</h2>
+                <ul style="text-align: left">
+                    <li><s>Anti Virus ...</s> Now it needs good pictures <a href="../software/antivirus#scroll">check it out</a> <a href="../español/software/antivirus">Dale Click</a></li>
+                    <li>MLWC same they will only have image placeholders for the time being.</li>
+                    <li>En Español: Mejora los clicks que para que el botón de ingles y español funcionen sin problema.</li>
+                    <li><!-- <s>Hardware: PCI card design...</s> <a href="../hardware/?pci#scroll">check it out</a>--></li>
+                    <li>Software: Page improvements...</li>
+                    <li></li>
+                    <li>Reviews: design a reviews page before migrating existing data.</li>
+                </ul>
+                <br />
                 <h2>TO-DO LIST 8/26/2024</h2>
                 <ul style="text-align: left">
                     <li><s>Backups Page...</s> <a href="../?backups#scroll">check it out</a> <a href="../español/?respaldos#scroll">Dale Click</a></li>
-                    <li>Home: a better footer maybe?</li>
-                    <li>En Español: pagina and page are undistinguishable twins.</li>
+                    <li><s>Home: a better footer maybe?</s></li>
+                    <li>En Español: pagina and page are undistinguishable twins. <a href="../">click on the Spanish and English button</a></li>
                     <li><s>Hardware: PCI card design...</s> <a href="../hardware/?pci#scroll">check it out</a></li>
-                    <li>Software: Program descriptions...</li>
-                    <li>Contact Us. Make it more inviting. (design)</li>
+                    <li><s>Software: Program descriptions...</s><a href="../software#ps2">check it out</a></li>
+                    <li><s>Contact Us. Make it more inviting. (design)</s> <a href="../contact">check it out</a></li>
                     <li>Reviews: design a reviews page before migrating existing data.</li>
                 </ul>
                 <h3>TECHNICAL</h3>
@@ -184,7 +195,7 @@ function content(){ ?>
                     and respaldos.php.
                 </p>
                 <h3>PROGRAMS</h3>
-                <p>0 0 0 1 1 1 1</p>
+                <p>Here the programs are coming along, we are moving as fast as possible ans we are working on a web browsers page at the moment. </p>
                 <h3>ANATOMY</h3>
                 <p>0 0 0 1 1 1 1</p>
                 <h3>NoteBooks</h3>
@@ -313,50 +324,50 @@ function content(){ ?>
                      button('#hardware');
                 ?><br />
                 <h2>Software</h2>
-                <p>Pomona, CA | 909-276-7214 | help@latinospc.com | 8/20/2024</p>
+                <p>Pomona, CA | 909-276-7214 | help@latinospc.com | 11/21/2024</p>
                 <p>Software page</p>
 
                 <aside style="text-align: left">
                     <h2>Open Source</h2>
-                    <p>0 1</p>
+                    <p>This type of software is freely available for use, modification, and distribution. The source code is also available for users to view and modify.</p>
                     <h2>Free</h2>
-                    <p>0 1 1</p>
+                    <p>This type of software is also freely available for use, but the source code may not be available for modification.</p>
                     <h2>Shareware</h2>
-                    <p>0 1 1</p>
+                    <p>This type of software is typically available for a free trial period, after which the user must purchase a license to continue using the software.</p>
                     <h2>Trial-ware</h2>
-                    <p>0 1 1</p>
+                    <p>Similar to shareware, this type of software is available for a free trial period, but typically includes more limitations or reduced functionality.</p>
                     <h2>Paid</h2>
-                    <p>0 1 1</p>
+                    <p>This type of software must be purchased in order to use it.</p>
                     <h2>Subscription</h2>
-                    <p>0 1 1</p>
+                    <p>This type of software requires a recurring payment (such as a monthly or annual fee) in order to continue using the software. Examples include software as a service (SaaS) applications.</p>
                     <h2>Anti Virus</h2>
-                    <p>0 1 1</p>
+                    <p>An anti-virus program in itself, is created to help the computer shield itself from viruses. The best antivirus solutions are the ones keeping Up to date with whats out there trying to harm the computer (PC). Also, the best antivirus solutions are simple, cost less than imagined, and bring peace of mind.</p>
                     <h2>Malware Cleaner</h2>
-                    <p>0 1 1</p>
+                    <p>Malware, harmful files infecting computers, can be found in spam, emails, infected drives, or bundled software. Often disguised as harmless software, malware steals data and can cause irreparable damage. To prevent infections, use paid or free antivirus solutions that offer real-time protection and background scanning.</p>
                     <h2>Web Browsers</h2>
-                    <p>0 1 1</p>
+                    <p>Web browsers, essential tools for accessing the internet, have evolved significantly since their early days. From text-based interfaces to modern graphical browsers, they have played a crucial role in shaping the online world. Today, browsers offer a wide range of features, from speed and security to customization options.</p>
                     <h2>Messaging</h2>
-                    <p>0 1 1</p>
+                    <p>Instant messaging enables fast communication over the internet beyond email. Modern apps support file sharing, group chats, and video/voice calls. They also allow users to make payments and play games. While the term originated in the 1990s, the concept dates back to the 1960s on multi-user operating systems like CTSS</p>
                     <h2>Media</h2>
-                    <p>0 1 1</p>
+                    <p>Media refers to computer representations of visual and audio formats, such as pictures, videos, and audio files. These formats shape how we interact with the digital world. From NFTs and video sites to books and image galleries, media spans various forms and enhances communication across the internet.</p>
                     <h2>Run-times</h2>
-                    <p>0 1 1</p>
+                    <p>Runtimes enable computer programs to execute across different systems, handling code execution for various programming languages. Lighter software generally runs faster than heavier programs requiring more system resources. As technology advances, mobile devices and laptops are improving, though they still lag behind desktops in processing power.</p>
                     <h2>Imaging</h2>
-                    <p>0 1 1</p>
+                    <p>Humans perceive the world through senses like sight, interpreting light to form reality. In contrast, computers generate 2D images, calculating how objects appear on screens. Imaging refers to the file formats used to create these visuals. Today, computers can also bring digital designs into the physical world via 3D printing.</p>
                     <h2>Documents</h2>
-                    <p>0 1 1</p>
+                    <p>Many types of computer files are classified as document files, used to store and organize information. Common examples in an office environment include text files (.txt), Word documents (.doc/.docx), spreadsheets (.xls/.xlsx), presentations (.ppt/.pptx), and PDFs (.pdf). These formats enable efficient data sharing, editing, and presentation across various platforms.</p>
                     <h2>File Sharing</h2>
-                    <p>0 1 1</p>
+                    <p>File sharing enables easy transfer of files between devices via cloud storage, email, or peer-to-peer networks. Popular services include Dropbox, Google Drive, and WeTransfer. Screen sharing tools like Zoom and Google Meet aid collaboration, while secure policies and password management tools ensure safety when sharing sensitive credentials.</p>
                     <h2>Online Storage</h2>
-                    <p>0 1 1</p>
+                    <p>Online storage, or cloud storage, allows users to store and access files on remote servers via the internet, offering flexibility across devices. Popular services like Google Drive, Dropbox, and OneDrive provide free storage with options to purchase more. Additional features include file sharing, collaboration tools, and backup services.</p>
                     <h2>Other</h2>
-                    <p>0 1 1</p>
+                    <p>Computer repair involves diagnosing and fixing various hardware and software issues. Basic troubleshooting steps include checking cable connections, ensuring the power supply is on, and verifying hardware components. If these steps fail, professional technicians can provide accurate diagnostics and solutions. Remote support services offer prompt, cost-effective technical assistance.</p>
                     <h2>Utilities</h2>
-                    <p>0 1 1</p>
+                    <p>Computer utilities manage, maintain, and optimize performance by cleaning hard drives, defragmenting disks, repairing errors, and enhancing security. Examples include antivirus software, disk cleanup tools, and system optimizers. These utilities, either built-in or standalone, improve speed and efficiency, ensuring smooth operation and preventing issues.</p>
                     <h2>Compression</h2>
-                    <p>0 1 1</p>
+                    <p>File compression reduces file size for storage and transmission. Early methods like run-length encoding evolved into sophisticated algorithms such as LZW, used in GIF and ZIP formats. Modern compression includes MP3 and JPEG. Common formats include ZIP, RAR, TAR, GZ, and BZ2, enhancing efficiency and performance.</p>
                     <h2>Developer Tools </h2>
-                    <p>0 1 1</p>
+                    <p>Here at LAtinosPC, developers enhance the website to be advanced yet user-friendly. They refine code for clarity and organization, ensuring a seamless browsing experience. Utilizing tools like Google Keep, WPS Office, Notepad++, GIMP, and WAMP, they create a better site for visitors, supported by resources like FileHippo and SourceForge.</p>
                 </aside>
                 <?php
                     echo button('#contact');
@@ -370,7 +381,7 @@ function content(){ ?>
                      button('#software');
                 ?><br />
                 <h2>Contact US</h2>
-                <p>Pomona, CA | 909-276-7214 | help@latinospc.com | 8/26/2024</p>
+                <p>Pomona, CA | 909-276-7214 | help@latinospc.com | 11/21/2024</p>
                 <p>Contact us page is using a php form called Simple PHP Contact Form master.<br />
                     The contact directory hosts the contact form in 3 separate directories.
                     <ul>
@@ -383,11 +394,25 @@ function content(){ ?>
 
                 <aside style="text-align: left">
                     <h2>Contact Us</h2>
-                    <p>0 1</p>
+                    <p>Contact Us
+Let us know how we are doing!
+Or leave us a review, we will be sure to post it.</p>
                     <h2>LATPC</h2>
-                    <p>0 1 1</p>
+                    <p>LAtinosPC.com
+Tel. 909-276-7214
+E-mail:LAtinosPC@gmail.com</p>
                     <h2>Contact Box</h2>
-                    <p>0 1 1</p>
+                    <p>Name
+Name
+Email
+Email
+Phone
+Phone
+Subject
+Subject
+Message
+Message
+<a href="../contact">Send</a></p>
                 </aside>
                 <?php
                     echo button('#reviews');
@@ -400,7 +425,7 @@ function content(){ ?>
             <?php
                      button('#contact');
                 ?><br />
-                <h2>Contact US</h2>
+                <h2>REVIEWS</h2>
                 <p>Pomona, CA | 909-276-7214 | help@latinospc.com | 8/20/2024</p>
                 <p>Contact us page</p>
 
